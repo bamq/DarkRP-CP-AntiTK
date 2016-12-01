ded2d393b9afce185d55a8e804c539b3a4c07ed3
bamq <bamq47@gmail.com>
2016-11-30 20:58:01 -0600

    Added variable for printing to console, other changes

--- lua/autorun/sv_antiteamkill.lua
+++ lua/autorun/sv_antiteamkill.lua
@@ -1,19 +1,23 @@
 
 if CLIENT then return end
 
-print( "[CP AntiTK] Enabled\n" )
+MsgN( "[CP AntiTK] Active - created by bamq." )
+
+-- Output to console when a Civil Protection attempts to damage another Civil Protection.
+local should_notify = true
 
 -- Anti-TeamKill for Civil Protection jobs.
-function CPAntiTK( vict, att )
+hook.Add( "PlayerShouldTakeDamage", "CPAntiTK_PlayerShouldTakeDamage", function( vict, att )
 
 	if vict:IsPlayer() and att:IsPlayer() and IsValid( vict ) and IsValid( att ) then
 	
-		if ( vict:isCP() ) and ( att:isCP() ) and not ( vict == att ) then
-		
-			print( "[CP AntiTK] CP " .. att:Nick() .. " (" .. att:SteamID() .. ") attempted to damage CP " .. vict:Nick() .. " (" .. vict:SteamID() .. "), negating.\n" )
+		if vict:isCP() and att:isCP() and not vict == att then
+			if should_notify then
+				MsgN( "[CP AntiTK] CP " .. att:Nick() .. " (" .. att:SteamID() .. ") attempted to damage CP " .. vict:Nick() .. " (" .. vict:SteamID() .. "), negating." )
+			end
 			return false
-			
 		end
+
 	end
+
 end
-hook.Add("PlayerShouldTakeDamage", "cpantiteamkill", CPAntiTK )
