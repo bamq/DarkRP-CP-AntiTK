c835d3f47fa80ae8a50d66bf1c9fad39768a6e21
bamq <bamq47@gmail.com>
2016-12-03 17:03:33 -0600

    Few small changes

--- lua/autorun/sv_antiteamkill.lua
+++ lua/autorun/sv_antiteamkill.lua
@@ -11,7 +11,7 @@ hook.Add( "PlayerShouldTakeDamage", "CPAntiTK_PlayerShouldTakeDamage", function(
 
 	if vict:IsPlayer() and att:IsPlayer() and IsValid( vict ) and IsValid( att ) then
 	
-		if vict:isCP() and att:isCP() and not vict == att then
+		if vict:isCP() and att:isCP() and vict ~= att then
 			if should_notify then
 				MsgN( "[CP AntiTK] CP " .. att:Nick() .. " (" .. att:SteamID() .. ") attempted to damage CP " .. vict:Nick() .. " (" .. vict:SteamID() .. "), negating." )
 			end
