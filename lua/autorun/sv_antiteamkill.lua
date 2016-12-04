1fb973c7d246f8d232504b7443275d8fe9bb97c5
bamq <bamq47@gmail.com>
2016-12-04 12:06:22 -0600

    Combine if statements
    One really long if statement.

--- lua/autorun/sv_antiteamkill.lua
+++ lua/autorun/sv_antiteamkill.lua
@@ -9,14 +9,12 @@ local should_notify = true
 -- Anti-TeamKill for Civil Protection jobs.
 hook.Add( "PlayerShouldTakeDamage", "CPAntiTK_PlayerShouldTakeDamage", function( vict, att )
 
-	if vict:IsPlayer() and att:IsPlayer() and IsValid( vict ) and IsValid( att ) then
-	
-		if vict:isCP() and att:isCP() and vict ~= att then
-			if should_notify then
-				MsgN( "[CP AntiTK] CP " .. att:Nick() .. " (" .. att:SteamID() .. ") attempted to damage CP " .. vict:Nick() .. " (" .. vict:SteamID() .. "), negating." )
-			end
-			return false
+	if IsValid( vict ) and vict:IsPlayer() and vict:isCP() and IsValid( att ) and att:IsPlayer() and att:isCP() and vict ~= att then
+
+		if should_notify then
+			MsgN( "[CP AntiTK] CP " .. att:Nick() .. " (" .. att:SteamID() .. ") attempted to damage CP " .. vict:Nick() .. " (" .. vict:SteamID() .. "), negating." )
 		end
+		return false
 
 	end
 
