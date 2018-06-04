a1950f8e6037a2339e429e0b6b1be8de76ce1a81
Taxin2012 <mrtaxin2012@mail.ru>
2018-06-04 15:26:13 +0300

    Update darkrpcpantitk.lua

--- lua/autorun/server/darkrpcpantitk.lua
+++ lua/autorun/server/darkrpcpantitk.lua
@@ -17,7 +17,6 @@
 
 -------------------------------------------------------------------------
 * 
-* Updated 03 April 2017
 * Update by Taxin2012. (http://steamcommunity.com/id/Taxin2012/)
 * 
 * Features:
@@ -28,32 +27,15 @@
 * 
 //-----------------------------------------------------------------------]]--
 
+-- /// SOME TABLES /// --
+
 CPAntiTK = CPAntiTK or {}
-CPAntiTK.VERSION = "2.0 [Edited by Taxin2012]"
 CPAntiTK.Config = CPAntiTK.Config or {}
-
--- /// CONFIG /// --
-
-CPAntiTK.Config.LogToConsole			= true --Should a message be printed in console when this event occurs? / Показывать сообщение в консоли?
-CPAntiTK.Config.CanDamage				= false --Can CP damaged other CP? / Сможет ли нанести урон один CP другому CP?
-
-
--- /// Demote System Config /// --
-CPAntiTK.Config.DemoteSystem			= false --Demote system / Система увольнения
-
-CPAntiTK.Config.Notify					= true	 --Make a notification on server for all CPs when CP attack other CP? / Сделать уведомление на сервере для всех CP, когда CP атакует другого CP?
-CPAntiTK.Config.JobsToBan = { TEAM_POLICE, TEAM_SWAT, TEAM_MAYOR } --Teams that will be banned / Работы, которые будут забанены
-CPAntiTK.Config.BanTime = 1200 --Demote time / Время Разбана профессии
-CPAntiTK.Hits = 10 --Number of hits / Кол-во попаданий
--- /// Demote System Config /// --
-
-
--- /// LANGUAGE /// --
 CPAntiTK.lang = {}
 CPAntiTK.lang.select ={}
-CPAntiTK.lang.select = 2		--Change this number to change language / Измените это значение для смены языка
 CPAntiTK.lang[1] = {}			--English / Английский
 CPAntiTK.lang[2] = {}			--Russian / Русский
+CPAntiTK.VERSION = "2.0 [Edited by Taxin2012]"
 
 CPAntiTK.lang[1] = {	--ENGLISH / Английский
 	
@@ -72,9 +54,27 @@ CPAntiTK.lang[2] = {	--Russian / Русский
 	demoted = " был уволен!  Причина: Стрельба по союзникам.",
 		
 }
--- /// LANGUAGE /// --
+
+-- /// END OF SOME TABLES /// --
 
 -- /// CONFIG /// --
+
+--Demote Config
+CPAntiTK.Config.DemoteSystem			= false --Demote system / Система увольнения
+
+CPAntiTK.Config.Notify					= true --Make a notification on server for all CPs when CP attack other CP? / Сделать уведомление на сервере для всех CP, когда CP атакует другого CP?
+CPAntiTK.Config.JobsToBan 				= { TEAM_POLICE, TEAM_SWAT, TEAM_MAYOR } --Teams that will be banned / Работы, которые будут забанены
+CPAntiTK.Config.BanTime 				= 1200 --Demote time / Время Разбана профессии
+CPAntiTK.Hits 							= 10 --Number of hits / Кол-во попаданий
+
+--Language Config
+CPAntiTK.lang.select 					= 1	--Change this number to change language / Измените это значение для смены языка
+
+--Other
+CPAntiTK.Config.LogToConsole			= true --Should a message be printed in console when this event occurs? / Показывать сообщение в консоли?
+CPAntiTK.Config.CanDamage				= false --Can CP damaged other CP? / Сможет ли нанести урон один CP другому CP?
+
+-- /// END OF CONFIG /// --
 
 
 MsgN( "DarkRP CP AntiTK v" .. CPAntiTK.VERSION .. " initialized. Created by bamq." )
@@ -82,41 +82,42 @@ MsgN( "DarkRP CP AntiTK v" .. CPAntiTK.VERSION .. " initialized. Created by bamq
 hook.Add( "PlayerShouldTakeDamage", "CPAntiTK_PlayerShouldTakeDamage", function( vict, att )
 	if IsValid( vict ) and vict:IsPlayer() and vict:isCP() and IsValid( att ) and att:IsPlayer() and att:isCP() and vict ~= att then
 		if CPAntiTK.Config.DemoteSystem then
-				att:SetNWInt( "cpantitk_hits", att:GetNWInt( "cpantitk_hits" ) + 1 )
-				if CPAntiTK.Config.Notify then
+			att:SetNWInt( "cpantitk_hits", att:GetNWInt( "cpantitk_hits" ) + 1 )
+			if CPAntiTK.Config.Notify then
 				for k,v in pairs( player.GetAll() ) do
-				if IsValid( v ) and v:IsPlayer() and v:isCP() then
-				DarkRP.notify(v, 1, 4, att:Nick()..CPAntiTK.lang[CPAntiTK.lang.select].attacked..vict:Nick().."!")
+					if IsValid( v ) and v:IsPlayer() and v:isCP() then
+						DarkRP.notify(v, 1, 4, att:Nick()..CPAntiTK.lang[CPAntiTK.lang.select].attacked..vict:Nick().."!")
+					end
 				end
-				end
-				end
-				DarkRP.notify(att, 1, 4, CPAntiTK.lang[CPAntiTK.lang.select].warned..att:GetNWInt( "cpantitk_hits" ).."/"..CPAntiTK.Hits)
+			end
+			DarkRP.notify(att, 1, 4, CPAntiTK.lang[CPAntiTK.lang.select].warned..att:GetNWInt( "cpantitk_hits" ).."/"..CPAntiTK.Hits)
 		end
 		if CPAntiTK.Config.DemoteSystem then
 		   if att:GetNWInt( "cpantitk_hits" ) >= CPAntiTK.Hits then
-							att:StripWeapons()
-							for _, team in pairs( CPAntiTK.Config.JobsToBan ) do
-							att:teamBan(team, CPAntiTK.Config.bantime)
-							end
-						timer.Simple(5,function() DarkRP.notify(att, 1, 4, CPAntiTK.lang[CPAntiTK.lang.select].willbedemote) end)
-						timer.Simple(7,function()
-							att:changeTeam( GAMEMODE.DefaultTeam, true )
-							att:SetNWInt( "cpantitk_hits" )
-							for k,v in pairs( player.GetAll() ) do
+				att:StripWeapons()
+					for _, team in pairs( CPAntiTK.Config.JobsToBan ) do
+						att:teamBan(team, CPAntiTK.Config.bantime)
+					end
+				timer.Simple(5,function() DarkRP.notify(att, 1, 4, CPAntiTK.lang[CPAntiTK.lang.select].willbedemote) end)
+				timer.Simple(7,function()
+					att:changeTeam( GAMEMODE.DefaultTeam, true )
+					att:SetNWInt( "cpantitk_hits" )
+						for k,v in pairs( player.GetAll() ) do
 							DarkRP.notify(v, 1, 4, att:Nick()..CPAntiTK.lang[CPAntiTK.lang.select].demoted)
-							end
-						end)
+						end
+				end)
 		    end
 		end
-				if CPAntiTK.Config.LogToConsole then
-					MsgN( "[DarkRP CP AntiTK] CP " .. att:Nick() .. " (" .. att:SteamID() .. ") attempted to damage CP " .. vict:Nick() .. " (" .. vict:SteamID() .. "), negating." )
-				end
-				if not CPAntiTK.Config.CanDamage then 
-				return false
-				else
-				return true
-				end
+		if CPAntiTK.Config.LogToConsole then
+			MsgN( "[DarkRP CP AntiTK] CP " .. att:Nick() .. " (" .. att:SteamID() .. ") attempted to damage CP " .. vict:Nick() .. " (" .. vict:SteamID() .. "), negating." )
+		end
+		
+		if not CPAntiTK.Config.CanDamage then 
+			return false
+		else
+			return true
+		end
 	end
-end )
+end)
 
 -- Created by bamq. Edited by Taxin2012
